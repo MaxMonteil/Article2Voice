ce7b4face746bcc301711222a361ae40023a0415
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-01-17 23:43:04 +0200

    Implement Google Cloud Text to Speech api on our parsed article content.

---
+++ server/texttospeech.py
@@ -0,0 +1,33 @@
+"""Synthesizes speech from the input string of text."""
+from google.cloud import texttospeech
+
+
+def article_to_speech(article):
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.types.SynthesisInput(text=article['text'])
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+    # The response's audio_content is binary.
+    return response.audio_content
+
+
+def generate_mp3_file(buff, binary_audio):
+    # write() returns number of bytes written, useful for
+    # Content-Length header
+    return buff.write(binary_audio)
