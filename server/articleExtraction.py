b41d3a8fcbf196430ffe8653ed625e3f2467854d
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-01-17 23:44:58 +0200

    Comments and formatting.

--- server/articleExtraction.py
+++ server/articleExtraction.py
@@ -1,13 +1,14 @@
+"""Newspaper abstraction to return text extracted from given URL."""
 from newspaper import Article
 
 
 def extract_article(url):
+    """Parses the article at the given URL and returns text and title."""
     # Initialize article object
     article = Article(
                 url=url,
                 language='en',
-                fetch_images=False
-            )
+                fetch_images=False)
     article.download()
     article.parse()
 
