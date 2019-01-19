6b7b0af034ae5e2a4adc93b89c673d3587edbd2a
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-01-19 10:23:28 +0200

    Add flask_cors library. Send just text to text to speech function

--- server/api.py
+++ server/api.py
@@ -1,17 +1,20 @@
-"""Standard application API"""
+'''Standard application API'''
 import io
+import sys
 from flask import Flask, jsonify, make_response, request, abort
+from flask_cors import CORS
 from articleExtraction import extract_article
 from texttospeech import article_to_speech, generate_mp3_file
 
 # create and configure the app
 app = Flask(__name__)
 app.config.from_mapping(SECRET_KEY='dev')
+CORS(app)
 
 
 @app.route('/')
 def index():
-    return jsonify({"message": "SUCCESS"}), 200
+    return jsonify({'message': 'This is an example'})
 
 
 @app.route('/api/v1/article', methods=['POST'])
@@ -19,18 +22,24 @@ def get_article():
     if not request.json or 'url' not in request.json:
         abort(400)
 
+    print('Getting article...', file=sys.stderr)
     article = extract_article(request.json['url'])
+    print('Article retrieved!\n', file=sys.stderr)
     # When the file name is sent as a header it can only have ascii characaters
     # .encode with ignore converts to ascii and removes the rest
     # this gives us a byte string so decode turns it back
     filename = (article['title'].encode('ascii', 'ignore').decode('utf-8') +
                 '.mp3')
 
-    binary_audio = article_to_speech(article)
+    print('Getting audio...', file=sys.stderr)
+    binary_audio = article_to_speech(article['text'])
+    print('Retrieved audio!\n', file=sys.stderr)
 
     # Buffer to keep file in memory instead of writing it to drive
     buff = io.BytesIO()
+    print(len(buff.getvalue()), file=sys.stderr)
     content_length = generate_mp3_file(buff, binary_audio)
+    print(len(buff.getvalue()), file=sys.stderr)
 
     response = make_response(buff.getvalue())
     buff.close()
