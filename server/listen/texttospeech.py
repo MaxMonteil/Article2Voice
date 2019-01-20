fe30ecd731d36d9e2d93c64690e8281675f15228
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-01-20 11:11:23 +0200

    Move to 'listen' folder and remove buffer writing function.

---
+++ server/listen/texttospeech.py
@@ -0,0 +1,27 @@
+"""Synthesizes speech from the input string of text."""
+from google.cloud import texttospeech
+
+
+def article_to_speech(article):
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.types.SynthesisInput(text=article)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+    # The response's audio_content is binary.
+    return response.audio_content
