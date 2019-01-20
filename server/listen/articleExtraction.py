06f1c3825a0a380e08aa6b1f4f9dcde7edd96977
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-01-20 11:12:07 +0200

    Move to 'listen' folder.

---
+++ server/listen/articleExtraction.py
@@ -0,0 +1,19 @@
+"""Newspaper abstraction to return text extracted from given URL."""
+from newspaper import Article
+
+
+def extract_article(url):
+    """Parses the article at the given URL and returns text and title."""
+    # Initialize article object
+    article = Article(
+                url=url,
+                language='en',
+                fetch_images=False,
+                memoize_articles=False)
+    article.download()
+    article.parse()
+
+    return {
+                'title': article.title,
+                'text': article.text
+           }
